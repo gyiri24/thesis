bfb2fa90337bb26367132e16e91ad22e0e071c2b
gyoridanidavid <gyiri24@gmail.com>
2023-02-19 18:33:04 +0100

    random number generate

--- Thesis/app/src/main/java/com/example/thesis/MainActivity.kt
+++ Thesis/app/src/main/java/com/example/thesis/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.thesis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.thesis.databinding.ActivityMainBinding
 import java.util.*
 
@@ -15,9 +16,29 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //DRY - Do not Repeat Yourself
+
+        var rand = Random()
+
         binding.btnTime.setOnClickListener{
-            binding.tvData.text = Date(System.currentTimeMillis()).toString()
+
+            if(binding.etData.text.isNotEmpty()) {
+                var num = rand.nextInt(100)
+
+                var currentTime = "$num ${binding.etData.text.toString()} ${Date(System.currentTimeMillis()).toString()}"
+
+                binding.tvData.text = currentTime
+
+                Toast.makeText(
+                    this,
+                    currentTime,
+                    Toast.LENGTH_LONG,
+                ).show()
+            } else {
+                binding.etData.error = "Ez nem lehet üres"
+            }
+
         }
-        
+
     }
 }
