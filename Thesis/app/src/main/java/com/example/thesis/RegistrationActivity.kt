f93a2900f52d93dacb6e252f5601d63682b37b71
gyoridanidavid <gyiri24@gmail.com>
2023-04-30 15:30:10 +0200

    terms and conditions dialog

--- Thesis/app/src/main/java/com/example/thesis/RegistrationActivity.kt
+++ Thesis/app/src/main/java/com/example/thesis/RegistrationActivity.kt
@@ -1,6 +1,7 @@
 package com.example.thesis
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.thesis.databinding.ActivityRegistrationBinding
 
@@ -13,6 +14,36 @@ class RegistrationActivity : AppCompatActivity() {
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.termsText.setOnClickListener {
+            showTermsDialog()
+        }
+
+        binding.privacyText.setOnClickListener {
+            showPrivacyDialog()
+        }
 
     }
+
+    private fun showTermsDialog() {
+        val termsDialog = AlertDialog.Builder(this)
+            .setTitle(getString(R.string.terms_text))
+            .setMessage(getString(R.string.terms_full_text))
+            .setPositiveButton(getString(R.string.ok)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+        termsDialog.show()
+    }
+
+    private fun showPrivacyDialog() {
+        val privacyDialog = AlertDialog.Builder(this)
+            .setTitle(getString(R.string.privacy_text))
+            .setMessage(getString(R.string.privacy_full_text))
+            .setPositiveButton(getString(R.string.ok)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+        privacyDialog.show()
+    }
+
 }
